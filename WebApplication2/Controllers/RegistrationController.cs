fde8b2252bc9c80d70080d5bf67627a6994a1eee
wael salameh <90796176+superwae@users.noreply.github.com>
2024-08-05 08:34:04 +0300

    Update RegistrationController.cs

--- WebApplication2/Controllers/RegistrationController.cs
+++ WebApplication2/Controllers/RegistrationController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication2.Controllers
@@ -12,23 +12,72 @@ namespace WebApplication2.Controllers
 
         public RegistrationController()
         {
+
+            FillStudents();
+            FillCourses();
+        }
+        void FillStudents()
+        {
             students = new List<Student>();
+            for(int i=0;i<20;i++)
+            {
+                var student = new Student();
+                student.Id = i;
+                student.Name = "A_" + i.ToString();
+                student.Dob = DateTime.Now;
+                student.IsActive = i % 2 == 0;
+                student.Address = new Address();
+                student.Address.Street = "street_" + i;
+                student.Address.City = "City_" + i;
+                student.Address.AddressDescription = "Description_" + i;
+                student.BuildingAddress = new List<Address>();
+                for (int j = 0; j < 3; j++)
+                {
+                    var buildingAddress = new Address();
+                    buildingAddress.AddressDescription = "Description_" + i + "_" + j;
+                    buildingAddress.City = "City_" + i + "_" + j;
+                    buildingAddress.Street = "Street_" + i + "_" + j;
+                    student.BuildingAddress.Add(buildingAddress);
+                }
+                students.Add(student);
+            }
+        }
+        void FillCourses()
+        {
             Courses = new List<Course>();
+            for (int i = 0; i < 20; i++)
+            {
+                var course = new Course();
+                course.Id = i;
+                course.Name = "Course_" + i.ToString();
+                course.SerialNumber = "SerialNumber_" + i;
+                Courses.Add(course);
+            }
         }
 
         [HttpPost]
         public IActionResult Register(Registration registration)
         {
-            var student = students.Find(c => c.Id == registration.StudentId);
-            var course = Courses.Find(c => c.Id == registration.CourseId);
-            if (student == null || course == null)
+            if(registration==null || registration.StudentId < 1 || registration.CourseId < 1)
             {
-                return BadRequest();
+                return BadRequest("invalid registration form");
             }
-            
-            //register 
+            var student = students.FirstOrDefault(x => x.Id == registration.StudentId);
+            var course = Courses.FirstOrDefault(x => x.Id == registration.CourseId);
+            if(student==null)
+            {
+                return NotFound("Student not found");
+            }
+            if(course==null)
+            {
+                return NotFound("course not found");
+            }
+            var reg = new Registration();
+            reg.CourseId = course.Id;
+            reg.StudentId = student.Id;
+            reg.RegistrationDate = DateTime.Now;
+            return Ok(reg);
 
-            return Ok(registration);
         }
     }
 }
